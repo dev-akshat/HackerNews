492966b71416ccc7f20a8ef6020dcb306e6e0dc9
akshatt004 <akshatt004.at@gmail.com>
2019-03-25 10:15:32 +0530

    Fake widget rendering

--- my_hacker_news/lib/src/screens/news_list.dart
+++ my_hacker_news/lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class NewsList extends StatelessWidget {
   @override
@@ -8,30 +7,7 @@ class NewsList extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Top News"),
       ),
-      body: buildList(),
+      body: new Text("Hello World"),
     );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return new Container(
-              height: 80.0,
-              child: snapshot.hasData
-                  ? Text("I'm visible $index")
-                  : Text("Data hasen't fetched yet at $index"),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(Duration(seconds: 2), () => "Hi");
   }
 }
